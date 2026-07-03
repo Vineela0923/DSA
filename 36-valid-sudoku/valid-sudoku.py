64c5248dcc8deb49a5c34be516e74b1d0e9bc117
Vineela Chilakamudi <vineelachilakamudi6@gmail.com>
2026-07-03 15:25:51 +0530

    Time: 4 ms (47.76%) | Memory: 19.2 MB (73.34%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen = set()
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+                if num != ".":
+                    if ((r, num) in seen or
+                        (num, c) in seen or
+                        (r // 3, c // 3, num) in seen):
+                        return False
+                    seen.add((r, num))
+                    seen.add((num, c))
+                    seen.add((r // 3, c // 3, num))
+        return True
+        
